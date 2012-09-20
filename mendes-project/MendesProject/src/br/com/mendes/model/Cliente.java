c09418a3d540c4c5b8dd46f79f2fe1fabd4b2591
joaoSakai <joao.sakai@gmail.com>
2012-09-20 08:12:40 -0300

    Up Project

---
+++ mendes-project/MendesProject/src/br/com/mendes/model/Cliente.java
@@ -0,0 +1,141 @@
+package br.com.mendes.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Pedro
+ */
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codCliente;
+    private String nome;
+    private String sobrenome;
+    @OneToMany
+    private Long codEndereco;
+    private String telefone;
+    private String celular;
+    private String rg;
+    private String cpf;
+    private String cnpj;
+    @Temporal(TemporalType.DATE)
+    private Date data_nascimento;
+    @Temporal(TemporalType.DATE)
+    private Date data_Cadastro;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long codCliente, String nome, String sobrenome, Long codEndereco, String telefone, String celular, String rg, String cpf, String cnpj, Date data_nascimento, Date data_Cadastro) {
+        this.codCliente = codCliente;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.codEndereco = codEndereco;
+        this.telefone = telefone;
+        this.celular = celular;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.cnpj = cnpj;
+        this.data_nascimento = data_nascimento;
+        this.data_Cadastro = data_Cadastro;
+    }
+
+    public Long getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(Long codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Long getCodEndereco() {
+        return codEndereco;
+    }
+
+    public void setCodEndereco(Long codEndereco) {
+        this.codEndereco = codEndereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public Date getData_Cadastro() {
+        return data_Cadastro;
+    }
+
+    public void setData_Cadastro(Date data_Cadastro) {
+        this.data_Cadastro = data_Cadastro;
+    }
+}
+
---
+++ mendes-project/MendesProject/src/br/com/mendes/model/Feedback.java
@@ -0,0 +1,112 @@
+package br.com.mendes.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Pedro
+ */
+@Entity
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codFeedback;
+    @ManyToOne
+    private Long codCliente;
+    @OneToOne
+    private Long codProduto;
+    @OneToOne
+    private Long codServico;
+    private double nota;
+    private String tipoAtendimento;
+    private String observação;
+    @Temporal(TemporalType.DATE)
+    private Date dataFeedback;
+
+    public Feedback() {
+    }
+
+    public Feedback(Long codFeedback, Long codCliente, Long codProduto, Long codServico, double nota, String tipoAtendimento, String observação, Date dataFeedback) {
+        this.codFeedback = codFeedback;
+        this.codCliente = codCliente;
+        this.codProduto = codProduto;
+        this.codServico = codServico;
+        this.nota = nota;
+        this.tipoAtendimento = tipoAtendimento;
+        this.observação = observação;
+        this.dataFeedback = dataFeedback;
+    }
+
+    public Long getCodFeedback() {
+        return codFeedback;
+    }
+
+    public void setCodFeedback(Long codFeedback) {
+        this.codFeedback = codFeedback;
+    }
+
+    public Long getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(Long codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public Long getCodProduto() {
+        return codProduto;
+    }
+
+    public void setCodProduto(Long codProduto) {
+        this.codProduto = codProduto;
+    }
+
+    public Long getCodServico() {
+        return codServico;
+    }
+
+    public void setCodServico(Long codServico) {
+        this.codServico = codServico;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public String getTipoAtendimento() {
+        return tipoAtendimento;
+    }
+
+    public void setTipoAtendimento(String tipoAtendimento) {
+        this.tipoAtendimento = tipoAtendimento;
+    }
+
+    public String getObservação() {
+        return observação;
+    }
+
+    public void setObservação(String observação) {
+        this.observação = observação;
+    }
+
+    public Date getDataFeedback() {
+        return dataFeedback;
+    }
+
+    public void setDataFeedback(Date dataFeedback) {
+        this.dataFeedback = dataFeedback;
+    }
+}
