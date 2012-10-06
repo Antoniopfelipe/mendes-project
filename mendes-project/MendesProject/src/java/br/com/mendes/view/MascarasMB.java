adedadec5eb0e19450cb8d8aa93220db3be21c7a
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2012-10-06 15:30:54 -0300

    Da uma olhada no converter !

---
+++ mendes-project/MendesProject/src/java/br/com/mendes/view/MascarasMB.java
@@ -0,0 +1,18 @@
+package br.com.mendes.view;
+
+public class MascarasMB {
+
+	
+	private String telefone;
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+}
