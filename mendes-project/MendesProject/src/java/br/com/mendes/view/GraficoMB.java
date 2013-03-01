e6ee14b262ad58fc58be4a8ff661ac89a5545a65
joaoSakai <joao.sakai@gmail.com>
2013-03-01 18:48:08 -0300

    Commit da tentativa de Retorno do Gráfico!

--- mendes-project/MendesProject/src/java/br/com/mendes/view/GraficoMB.java
+++ mendes-project/MendesProject/src/java/br/com/mendes/view/GraficoMB.java
@@ -1,26 +1,41 @@
 package br.com.mendes.view;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.faces.bean.RequestScoped;
+import javax.annotation.PostConstruct;
 
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.LineChartSeries;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import br.com.mendes.model.Categoria;
+import br.com.mendes.service.CategoriaService;
+
+
 @Component("graficoMB")
-@RequestScoped
+@Scope(value="request")
 public class GraficoMB implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private CartesianChartModel linearModel;
-
-	public GraficoMB() {
-		createLinearModel();
-		
+	private List<Categoria> categorias;
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@PostConstruct
+	public void iniciar() {
+		//categorias = categoriaService.obterTodasCategorias();
 	}
 
+	public GraficoMB(){
+		createLinearModel();
+	}
+	
 	public CartesianChartModel getLinearModel() {
 		return linearModel;
 	}
@@ -30,35 +45,21 @@ public class GraficoMB implements Serializable {
 
 		LineChartSeries series1 = new LineChartSeries();
 		series1.setLabel("Homens");
-
-		series1.set(1, 2);
-		series1.set(2, 1);
-		series1.set(3, 3);
-		series1.set(4, 6);
-		series1.set(5, 8);
-
-		LineChartSeries series2 = new LineChartSeries();
-		series2.setLabel("Mulheres");
-		series2.setMarkerStyle("diamond");
-
-		series2.set(1, 6);
-		series2.set(2, 3);
-		series2.set(3, 2);
-		series2.set(4, 7);
-		series2.set(5, 9);
-
-		LineChartSeries series3 = new LineChartSeries();
-		series3.setLabel("Filhos");
-
-		series3.set(1, 3);
-		series3.set(2, 5);
-		series3.set(3, 3);
-		series3.set(4, 8);
-		series3.set(5, 8);
-
+		
+		Categoria cate = new Categoria();
+		cate = criarCategoria(cate);
+		series1.set(1, cate.getCodCategoria() );
+		
+	
 		linearModel.addSeries(series1);
-		linearModel.addSeries(series2);
-		linearModel.addSeries(series3);
+	}
+
+	public Categoria criarCategoria(Categoria cat){
+		 cat = new Categoria();
+		cat.setCodCategoria(100L);
+		cat.setDescricao("TESTE");
+		return cat;
+		
 	}
 
 }
