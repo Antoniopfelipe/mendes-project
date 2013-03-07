c22ab58edf4bd3e87d2683d098d2286ee8958e40
joaoSakai <joao.sakai@gmail.com>
2013-03-07 16:10:39 -0300

    Tela de Pedido

--- mendes-project/MendesProject/src/java/br/com/mendes/view/PedidoMB.java
+++ mendes-project/MendesProject/src/java/br/com/mendes/view/PedidoMB.java
@@ -1,6 +1,7 @@
 package br.com.mendes.view;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -12,10 +13,13 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import br.com.mendes.model.Cliente;
+import br.com.mendes.model.Item;
 import br.com.mendes.model.Pedido;
 import br.com.mendes.model.Produto;
 import br.com.mendes.model.Servico;
+import br.com.mendes.model.TipoItem;
 import br.com.mendes.service.ClienteService;
+import br.com.mendes.service.ItemService;
 import br.com.mendes.service.PedidoService;
 import br.com.mendes.service.ProdutoService;
 import br.com.mendes.service.ServicoService;
@@ -30,11 +34,15 @@ public class PedidoMB implements Serializable {
 	private Cliente cliente;
 	private Produto produto;
 	private Servico servico;
+	private TipoItem tipoItem;
+	private Integer quantidade;
 
 	private List<Pedido> pedidos;
 	private List<Cliente> clientes;
 	private List<Produto> produtos;
 	private List<Servico> servicos;
+	private List<TipoItem> tiposItem;
+	private List<Item> itens;
 
 	@Autowired
 	private PedidoService pedidoService;
@@ -47,20 +55,33 @@ public class PedidoMB implements Serializable {
 
 	@Autowired
 	private ServicoService servicoService;
+	
+	@Autowired
+	private ItemService itemService;
 
 	@PostConstruct
 	public void iniciar() {
 
+	
+		
 		pedidos = pedidoService.obterTodosPedidos();
 		clientes = clienteService.obterTodosCliente();
 		produtos = produtoService.obterTodosProduto();
 		servicos = servicoService.obterTodosServicos();
+		
 	}
 
 	public PedidoMB() {
 		pedido = new Pedido();
 		pedido.setCliente(new Cliente());
 		pedido.setServico(new Servico());
+		
+		tiposItem = Arrays.asList(TipoItem.values());	
+	}
+	
+	public void escolherTipoItem() {
+		
+		itens = itemService.buscarPorTipoECLiente(cliente.getCodCliente(), tipoItem);
 	}
 
 	public void salvarPedido() {
@@ -73,6 +94,7 @@ public class PedidoMB implements Serializable {
 
 		pedidos = pedidoService.obterTodosPedidos();
 	}
+	
 
 	public Pedido getPedido() {
 		return pedido;
@@ -136,6 +158,38 @@ public class PedidoMB implements Serializable {
 
 	public void setServicos(List<Servico> servicos) {
 		this.servicos = servicos;
+	}
+
+	public TipoItem getTipoItem() {
+		return tipoItem;
+	}
+
+	public void setTipoItem(TipoItem tipoItem) {
+		this.tipoItem = tipoItem;
+	}
+
+	public List<TipoItem> getTiposItem() {
+		return tiposItem;
+	}
+
+	public void setTiposItem(List<TipoItem> tiposItem) {
+		this.tiposItem = tiposItem;
+	}
+
+	public List<Item> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<Item> itens) {
+		this.itens = itens;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
 	}
 
 }
